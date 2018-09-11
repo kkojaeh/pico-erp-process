eb6a107d83694b9fe47748e90fe4e3d5f958c5ef
Ethan <kkojaeh@gmail.com>
2018-09-11 11:31:35 +0900

    메뉴 삭제

--- process-api/src/main/java/pico/erp/process/ROLE.java
+++ process-api/src/main/java/pico/erp/process/ROLE.java
@@ -1,38 +1,21 @@
 package pico.erp.process;
 
-import java.util.Collections;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import javax.persistence.Id;
-import javax.persistence.Transient;
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import pico.erp.shared.data.Menu;
 import pico.erp.shared.data.Role;
 
 @RequiredArgsConstructor
 public enum ROLE implements Role {
 
-  PROCESS_TYPE_MANAGER(
-    Stream.of(MENU.PROCESS_TYPE_MANAGEMENT, MENU.PREPROCESS_TYPE_MANAGEMENT)
-      .collect(Collectors.toSet())
-  ),
+  PROCESS_TYPE_MANAGER,
 
-  PROCESS_MANAGER(
-    Stream.of(MENU.PROCESS_MANAGEMENT)
-      .collect(Collectors.toSet())
-  ),
+  PROCESS_MANAGER,
 
-  PROCESS_ACCESSOR(Collections.emptySet());
+  PROCESS_ACCESSOR;
 
   @Id
   @Getter
   private final String id = name();
 
-  @Transient
-  @Getter
-  @NonNull
-  private Set<Menu> menus;
 }
