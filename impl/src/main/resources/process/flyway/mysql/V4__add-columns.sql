a6792a7bf302187bc7f233003a0dacfc7b1fa9dd
Ethan <kkojaeh@gmail.com>
2019-01-28 14:26:48 +0900

    DDL 추가

---
+++ impl/src/main/resources/process/flyway/mysql/V4__add-columns.sql
@@ -0,0 +1,6 @@
+ALTER TABLE prs_process ADD input_rate decimal(7,5) default 1 NOT NULL;
+ALTER TABLE prs_process ADD item_id binary(16) NOT NULL;
+ALTER TABLE prs_process ADD item_order integer default 0
+
+create index IDXe1vcflsrij5u2mrbpbp9wrayg
+	on prs_process (item_id);
